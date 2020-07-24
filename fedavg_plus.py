8936bcd7c84453bda7ac52fcde86473ef738f2be
KevinYang515 <KevinYang515>
2020-07-25 02:58:47 +0800

    First version

--- fedavg_plus.py
+++ fedavg_plus.py
@@ -20,12 +20,12 @@ import random
 import data.preprocessing as prep
 from data.read_data import read_data
 from data.data_utils import load_cifar10_data, train_test_label_to_categorical
-from model.model import define_model, init_compile
+from model.model import define_model, init_compile, init_model, record_history
+from model.operation import broadcast_to_device, caculate_delta, aggregate_add, aggregate_division_return 
 
 from math import floor
 from sklearn.utils import shuffle
 from random import randint
-from tensorflow.keras import layers, models
 from keras import backend as K
 from keras.preprocessing.image import ImageDataGenerator
 
@@ -58,9 +58,9 @@ def main(argv):
 
     # adjust parameters of the model
     callback = tf.keras.callbacks.LearningRateScheduler(step_decay)
-    
+
     augment = ImageDataGenerator(preprocessing_function=prep.preprocessing_for_training)
-    history_02 = {}
+    # history_total = {}
     # 30000
     # device_list = [0, 20, 21, 22, 23, 27, 37, 65, 73, 81, 95, 98]
 
@@ -75,107 +75,62 @@ def main(argv):
     num_device = 100
     num_center_epoch = 1
     num_local_epoch = 5
-    num_round = 2
+    num_round = 100
     center_batch_size = 64
     local_batch_size = 50
 
     show = 0
 
     # Define our model
-    model_m = define_model()
-    init_compile(model_m)
+    model_m, history_total = init_model(True)
 
-    #Initialization for all device's record
-    history_m = {}
-    for device in new_device_info:
-        locals()['history_{}'.format(device)] = {}
-
-    test_d = np.stack([prep.preprocessing_for_testing(test_images[i]) for i in range(10000)], axis=0)
-    history_temp = model_m.evaluate(test_d, test_label, batch_size=64)
-
-    # try and error history_m
-    history_02['val_loss'] = [history_temp[0]]
-    history_02['val_acc'] = [history_temp[1]]
-
+    device_set = [x for x in new_device_info]
     for _ in range(num_round):
         print("\n" + "\033[1m" + "Round: " + str(_))
         for device in new_device_info:
-            print("\033[0m" + "\nDevice:", device, "model_" + str(device))
+            print("\033[0m" + "Device:", device, "model_" + str(device))
 
-            if(_ == 0):
+            if (_ == 0):
                 #Define and initialize an estimator model
-                locals()['model_{}'.format(device)] = define_model()
-                init_compile(locals()['model_{}'.format(device)])
-                
-            #Broadcast to every device (e.g., model_m parameters to all devices)
-            for layer in range(len(model_m.layers)):
-                locals()['model_{}'.format(device)].layers[layer].set_weights(model_m.layers[layer].get_weights())
+                locals()['model_{}'.format(device)] = init_model(False)
+            else:
+                #Broadcast to every device (e.g., model_m parameters to all devices)
+                broadcast_to_device(locals()['model_{}'.format(device)], model_m)
 
             #Local training on each device 
             for epo in range(num_local_epoch):
-                print("Epoch:", epo)
                 train_image_temp, train_label_temp = prep.prepare_for_training_data0(train_images, train_labels, data_distribution, device)
                 train_image_crop = np.stack([prep.random_crop(train_image_temp[i], 24, 24) for i in range(len(train_image_temp))], axis=0)
-
-                test_image_temp, test_label_temp = prep.prepare_for_testing_data(test_images, test_labels, device, len(new_device_info))
-                test_image_crop = np.stack([prep.preprocessing_for_testing(test_image_temp[i]) for i in range(len(test_image_temp))], axis=0)
-
                 train_new_image, train_new_label = shuffle(train_image_crop, 
                                                         train_label_temp, 
                                                         random_state=randint(0, train_image_crop.shape[0]))
-                test_new_image, test_new_label = shuffle(test_image_crop, 
-                                                        test_label_temp, 
-                                                        random_state=randint(0, test_image_crop.shape[0]))
-
-    #             show = 1 if e == num_device else 0
 
                 history_temp = locals()['model_{}'.format(device)].fit_generator(
                     augment.flow(train_new_image, train_new_label, batch_size=local_batch_size), 
                     epochs=1, 
                     callbacks=[callback],
-                    verbose=show,
-                    validation_data=(test_new_image, test_new_label))
-
-                #Record accuracy of each device
-                for attribute in history_temp.history:
-                    if attribute in locals()['history_{}'.format(device)]:
-                        locals()['history_{}'.format(device)][attribute].extend(history_temp.history[attribute])
-                    else:
-                        locals()['history_{}'.format(device)][attribute] = history_temp.history[attribute]
-
+                    verbose=show)
 
             #Calculate delta weight on each device
-            for layer in range(len(model_m.layers)):
-                locals()['model_{}'.format(device)].layers[layer].set_weights(
-                    np.subtract(locals()['model_{}'.format(device)].layers[layer].get_weights(),
-                    model_m.layers[layer].get_weights()))
-                
+            caculate_delta(locals()['model_{}'.format(device)], model_m)
+
         #Aggregate all delta weight on device 0 (Addition)
-        for device in [x for x in new_device_info][1:]:
-            for layer in range(len(model_m.layers)):
-                model_0.layers[layer].set_weights(
-                    np.add(locals()['model_{}'.format(device)].layers[layer].get_weights(),
-                    model_0.layers[layer].get_weights()))
+        for device in device_set[1:]:
+            aggregate_add(locals()['model_{}'.format(device)], locals()['model_{}'.format(device_set[0])])
 
-        #Aggregate all delta weight on device 0 (Division)
-        for layer in range(len(model_m.layers)):
-            model_0.layers[layer].set_weights(np.divide(model_0.layers[layer].get_weights(), len(new_device_info)))
+        #Aggregate all delta weight on device 0 (Division) and return total delta weight to center device
+        aggregate_division_return(locals()['model_{}'.format(device_set[0])], model_m, len(new_device_info))
 
-        #Return total delta weight to center device
-        for layer in range(len(model_m.layers)):
-            model_m.layers[layer].set_weights(np.add(model_m.layers[layer].get_weights(),
-                                                    model_0.layers[layer].get_weights()))
         print("Result : " + str(_))
         #Evaluate with new weight
         test_d = np.stack([prep.preprocessing_for_testing(test_images[i]) for i in range(10000)], axis=0)
         test_new_image, test_new_label = shuffle(test_d, test_label, 
                                                 random_state=randint(1, train_images.shape[0]))
-        
+
         history_temp = model_m.evaluate(test_new_image, test_new_label, batch_size=64)
 
         #Record each round accuracy
-        history_02['val_loss'].append(history_temp[0])
-        history_02['val_acc'].append(history_temp[1])
-
+        record_history(history_temp, history_total)
+    
 if __name__ == '__main__':
     main(sys.argv)
--- model/model.py
+++ model/model.py
@@ -1,6 +1,23 @@
 import tensorflow as tf
 from tensorflow.keras import layers, models
 
+def init_model(is_master=False):
+    """
+    Initial model, compile, and load initial weight
+    If we want the master one, it will return model and history for storing accuracy and loss.
+    if we want the client one, it will return only model.
+    :param check the model we want master or client
+    :return initialized, compiled, loaded model
+    :return history dictionary (optional)
+    """
+    model_x = define_model()
+    init_compile(model_x)
+    init_load_weight(model_x)
+    if is_master == True:
+        return model_x, init_history()
+    else:
+        return model_x
+
 # Define our model
 def define_model():
     """
@@ -23,6 +40,36 @@ def define_model():
     ])
 
 def init_compile(model_x):
+    """
+    Compile the input model
+    :param model which we want to be compiled
+    """
     model_x.compile(optimizer='sgd',
             loss='categorical_crossentropy',
             metrics=['accuracy'])
+
+def init_load_weight(model_x):
+    """
+    Load initial weight for the input model
+    :param model which we want to load the initial weight
+    """
+    model_x.load_weights('model/weight/ini_cifar10.h5')
+
+def init_history():
+    """
+    Return the inital history dictionary to store accuracy and loss
+    :return history dictionary
+    """
+    history = {}
+    history['val_loss'] = [2.3840]
+    history['val_acc'] = [0.0976]
+    return history
+
+def record_history(history_temp, history_total):
+    """
+    Record accuracy and loss result to history_total
+    :param the dictionary stored the accuracy and loss for this round
+    :param the dictionary stored the accuracy and loss for all round 
+    """
+    history_total['val_loss'].append(history_temp[0])
+    history_total['val_acc'].append(history_temp[1])
